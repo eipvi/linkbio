c8ffed81a36ad154fedfa39acad525b0fdb28b64
Paula Vieira <eipvi@proton.me>
2024-01-24 23:44:56 -0300

    style: sidebar base

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
+import Avatar from "/src/public/img/paulav.jpeg";
+
+import "/src/sass/app.scss";
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
+<aside id="sidebar">
+<p>imagem</p>
+<h1>Paula V.</h1>
+<p className="title">Desenvolvedora e Ui/Ux Design</p>
+<p>Redes</p>
+<p>Informações de contato</p>
+<a href="" className="btn">Entre em contato</a>
+</aside>  )
 }
 
 export default Sidebar
