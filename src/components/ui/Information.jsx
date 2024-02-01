c98d828f0d6d2af33be86752a982ee1a3b4bcbda
Paula Vieira <eipvi@proton.me>
2024-01-31 23:50:08 -0300

    style: estilo card e componente sidebar

--- src/components/ui/Information.jsx
+++ src/components/ui/Information.jsx
@@ -1,6 +1,24 @@
+import { AiOutlineMail, AiFillEnvironment} from "react-icons/ai";
+import "/src/sass/app.scss";
+
 const Information = () => {
   return (
-    <div>Information</div>
+   <section id="information">
+      <div className="info-card">
+        <AiOutlineMail id="email-icon" />
+         <div>
+            <h3>E-mail</h3>
+            <p>paula@iversee.com</p>
+         </div>
+    </div>
+      <div className="info-card">
+        <AiFillEnvironment id="pin-icon" />
+         <div>
+            <h3>Localização</h3>
+            <p>Brasil/MG</p>
+         </div>
+    </div>
+   </section>
   )
 }
 
